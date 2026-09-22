0aee0fba696ee815f2b2c8b1be9ff72005cf7e0e
kaydos07 <hoanghuyd45@gmail.com>
2026-09-22 21:17:50 +0700

    Add customer and pet`s Configuration
    Add Configuration for pet and customer identity to config rule for value

---
+++ src/PetSpa.Modules/Customer/Infrastructure/Persistence/Configurations/CustomerConfiguration.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+using CustomerTable = PetSpa.Modules.Customer.Domain.Entities.Customer;
+
+namespace PetSpa.Modules.Customer.Infrastructure.Persistence.Configurations
+{
+    public class CustomerConfiguration : IEntityTypeConfiguration<CustomerTable>
+    {
+        public void Configure(EntityTypeBuilder<CustomerTable> builder)
+        {
+            builder.ToTable("customer");
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.FullName)
+                .HasColumnName("fullname")
+                .IsRequired()
+                .HasMaxLength(50);
+
+            builder.Property(x => x.Gender)
+                .HasColumnName("gender")
+                .HasMaxLength(10);
+
+            builder.Property(x => x.Date_of_Birth)
+                .HasColumnName("date_of_birth")
+                .HasColumnType("date");
+
+            builder.Property(x => x.Email)
+                .HasColumnName("email")
+                .IsRequired()
+                .HasMaxLength(50);
+            builder.HasIndex(x => x.Email).IsUnique();
+
+            builder.Property(x => x.Phone)
+                .HasColumnName("phone")
+                .HasMaxLength(20);
+            builder.HasIndex(x => x.Phone).IsUnique();
+
+            builder.Property(x => x.PasswordHash)
+                .HasColumnName("password_hash")
+                .IsRequired()
+                .HasMaxLength(256);
+
+            builder.Property(x => x.Avt_Url)
+                .HasColumnName("avt_url")
+                .HasMaxLength(256);
+
+            builder.Property(x => x.Note)
+                .HasColumnName("note")
+                .HasMaxLength(256);
+
+            builder.Property(x => x.Status) 
+                .HasColumnName("status")
+                .IsRequired()
+                .HasMaxLength(20);
+
+            builder.Property(x => x.Created_at)
+                .HasColumnName("created_at")
+                .HasColumnType("timestamp with time zone");
+
+            builder.Property(x => x.Updated_at)
+                .HasColumnName("updated_at")
+                .HasColumnType("timestamp with time zone");
+
+            builder.Property(x=> x.Last_login_at)
+                .HasColumnName("last_login_at")
+                .HasColumnType("timestamp with time zone");
+        }
+    }
+}
---
+++ src/PetSpa.Modules/Customer/Infrastructure/Persistence/Configurations/PetConfiguration.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using PetSpa.Modules.Customer.Domain.Entities;
+
+namespace PetSpa.Modules.Customer.Infrastructure.Persistence.Configurations
+{
+    public class PetConfiguration: IEntityTypeConfiguration<Pet>
+    {
+        public void Configure(EntityTypeBuilder<Pet> builder)
+        {
+            builder.ToTable("pet");
+
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.CustomersId)
+                .HasColumnName("customer_id");
+
+            builder.HasOne(x => x.Customer)
+                .WithMany(y => y.Pets)
+                .HasForeignKey(z => z.CustomersId)
+                .HasConstraintName("fk_pet_customer")
+                .OnDelete(DeleteBehavior.Cascade); // auto delete Pet when Customer is deleted
+
+            builder.Property(x => x.Name)
+                .HasColumnName("name")
+                .HasMaxLength(50)
+                .IsRequired();
+
+            builder.Property(x => x.Species)
+                .HasColumnName("species")
+                .HasMaxLength(50)
+                .IsRequired();
+
+            // No minValue so we will validate value later in Application.validator
+
+            builder.Property(x => x.Weight)
+                .HasColumnName("weight");
+
+            builder.Property(x => x.Height)
+                .HasColumnName("height");
+
+            builder.Property(x => x.Gender)
+                .HasColumnName("gender");
+
+            builder.Property(x => x.Date_of_Birth)
+                .HasColumnName("date_of_birth")
+                .HasColumnType("date");
+
+            builder.Property(x => x.Status)
+                .HasColumnName("status")
+                .HasMaxLength(10)
+                .IsRequired();
+
+            builder.Property(x => x.Special_note)
+                .HasColumnName("special_note")
+                .HasColumnType("text");
+
+            builder.Property(x => x.Created_at)
+                .HasColumnName("created_at")
+                .HasColumnType("timestamp with time zone");
+
+            builder.Property(x => x.Updated_at)
+                .HasColumnName("updated_at")
+                .HasColumnType("timestamp with time zone");
+        }
+    }
+}
