891fa0a08dc0f4bea36a2d50b42f3259d99047d3
kaydos07 <hoanghuyd45@gmail.com>
2026-09-24 23:16:38 +0700

    feat(Operation): add Entities

---
+++ src/PetSpa.Modules/Operation/Domain/Entities/StaffAssignment.cs
@@ -0,0 +1,14 @@
+﻿
+
+namespace PetSpa.Modules.Operation.Domain.Entities
+{
+    public class StaffAssignment
+    {
+        public long Id { get; set; }
+        public long ServiceSessionId { get; set; }
+        public ServiceSession ServiceSession { get; set; } = null!;
+        public long StaffId { get; set; }
+        public Staff Staff { get; set; } = null!;
+        public DateTime? AssignedAt { get; set; }
+    }
+}
