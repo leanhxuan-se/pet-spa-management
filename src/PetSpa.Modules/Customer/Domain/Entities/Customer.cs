740460ff97840f360fb180b2ef3ee7e0aaca8d58
kaydos07 <hoanghuyd45@gmail.com>
2026-09-24 09:59:06 +0700

    Delete using DataAnnotations

--- src/PetSpa.Modules/Customer/Domain/Entities/Customer.cs
+++ src/PetSpa.Modules/Customer/Domain/Entities/Customer.cs
@@ -1,6 +1,4 @@
-﻿using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
-
+﻿
 namespace PetSpa.Modules.Customer.Domain.Entities
 {
     public class Customer
