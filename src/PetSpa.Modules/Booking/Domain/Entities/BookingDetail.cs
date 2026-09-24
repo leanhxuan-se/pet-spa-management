3fd52df26a07abca782da166a2ff3e6ba6f4470a
kaydos07 <hoanghuyd45@gmail.com>
2026-09-24 10:44:54 +0700

    Add Booking Module entity

---
+++ src/PetSpa.Modules/Booking/Domain/Entities/BookingDetail.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PetSpa.Modules.Booking.Domain.Entities
+{
+    public class BookingDetail
+    {
+        public long Id { get; set; }
+        public long BookingId { get; set; }
+        public Booking Booking { get; set; } = null!;
+        public long ServiceOptionsId { get; set; }
+        public long UnitPrice { get; set; }
+        public string Status { get; set; } = string.Empty;
+    }
+}
---
+++ src/PetSpa.Modules/Booking/Infrastructure/Persistence/Configurations/BookingConfiguration.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+using BookingEntity = PetSpa.Modules.Booking.Domain.Entities.Booking;
+
+namespace PetSpa.Modules.Booking.Infrastructure.Persistence.Configurations
+{
+    internal class BookingConfiguration: IEntityTypeConfiguration<BookingEntity>
+    {
+        public void Configure(EntityTypeBuilder<BookingEntity> Builder)
+        {
+            Builder.ToTable("booking");
+
+            Builder.HasKey(x => x.Id);
+
+            Builder.Property(x => x.CustomerId)
+                .HasColumnName("customer_id")
+                .IsRequired();
+
+            Builder.Property(x => x.PetId)
+                .HasColumnName("pet_id")
+                .IsRequired();
+
+            Builder.Property(x => x.BookingTime)
+                .HasColumnName("booking_time")
+                .HasColumnType("timestamp with time zone");
+
+            Builder.Property(x => x.CheckIn)
+                .HasColumnName("check_in")
+                .HasColumnType("timestamp with time zone");
+
+            Builder.Property(x => x.CheckOut)
+                .HasColumnName("check_out")
+                .HasColumnType("timestamp with time zone");
+
+            Builder.Property(x => x.Note)
+                .HasColumnName("note")
+                .HasColumnType("text");
+
+            Builder.Property(x => x.Status)
+                .HasColumnName("status")
+                .HasMaxLength(20);
+
+            Builder.Property(x => x.CreatedAt)
+                .HasColumnName("created_at")
+                .HasColumnType("timestamp with time zone");
+
+            Builder.Property(x => x.UpdatedAt)
+                .HasColumnName("updated_at")
+                .HasColumnType("timestamp with time zone");
+
+        }
+    }
+}
