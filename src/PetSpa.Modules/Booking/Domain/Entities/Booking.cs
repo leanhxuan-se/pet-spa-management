cfcbeefa072f5c882b34584c2d2c225ee4f86bd5
kaydos07 <hoanghuyd45@gmail.com>
2026-09-24 23:18:40 +0700

    Update entity properties

--- src/PetSpa.Modules/Booking/Domain/Entities/Booking.cs
+++ src/PetSpa.Modules/Booking/Domain/Entities/Booking.cs
@@ -6,13 +6,13 @@ namespace PetSpa.Modules.Booking.Domain.Entities
         public long Id { get; set; }
         public long CustomerId { get; set; }
         public long PetId { get; set; }
-        public DateTime BookingTime { get; set; }
+        public DateTime BookingTime { get; set; } = DateTime.UtcNow;
         public DateTime? CheckIn { get; set; }
         public DateTime? CheckOut { get; set; }
         public string? Note { get; set; }
         public string Status { get; set; } = string.Empty;
         public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
-        public DateTime UpdatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
 
         public ICollection<BookingDetail> BookingDetails { get; set; } = [];
         
--- src/PetSpa.Modules/Customer/Domain/Entities/Customer.cs
+++ src/PetSpa.Modules/Customer/Domain/Entities/Customer.cs
@@ -6,7 +6,7 @@ namespace PetSpa.Modules.Customer.Domain.Entities
         public long Id { get; set; }
         public string FullName { get; set; } = string.Empty;
         public string Gender { get; set; } = string.Empty;
-        public DateOnly? Date_of_Birth { get; set; }
+        public DateOnly? DateOfBirth { get; set; }
         public string Email { get; set; } = string.Empty;
         public string Phone { get; set; } = string.Empty;
         public string PasswordHash { get; set; } = string.Empty;
@@ -14,8 +14,8 @@ namespace PetSpa.Modules.Customer.Domain.Entities
         public string? Note { get; set; }
         public string Status { get; set; } = "Active";
         public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
-        public DateTime UpdatedAt { get; set; }
-        public DateTime LastLoginAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+        public DateTime? LastLoginAt { get; set; }
 
         // reference to get pets from a Customer, obligated for a 1 (Customer) : N (pet) relationship, will not create Pets column in database
         public ICollection<Pet> Pets { get; set; } = [];
