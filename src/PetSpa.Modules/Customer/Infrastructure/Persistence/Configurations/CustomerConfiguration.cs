33d328b7e342ed76f8ecd99785d7c08102de4647
kaydos07 <hoanghuyd45@gmail.com>
2026-09-25 20:40:55 +0700

    Fix Customer Configuration

--- src/PetSpa.Modules/Customer/Infrastructure/Persistence/Configurations/CustomerConfiguration.cs
+++ src/PetSpa.Modules/Customer/Infrastructure/Persistence/Configurations/CustomerConfiguration.cs
@@ -10,6 +10,7 @@ namespace PetSpa.Modules.Customer.Infrastructure.Persistence.Configurations
         public void Configure(EntityTypeBuilder<CustomerTable> builder)
         {
             builder.ToTable("customer");
+
             builder.HasKey(x => x.Id);
 
             builder.Property(x => x.FullName)
@@ -21,7 +22,7 @@ namespace PetSpa.Modules.Customer.Infrastructure.Persistence.Configurations
                 .HasColumnName("gender")
                 .HasMaxLength(10);
 
-            builder.Property(x => x.Date_of_Birth)
+            builder.Property(x => x.DateOfBirth)
                 .HasColumnName("date_of_birth")
                 .HasColumnType("date");
 
@@ -39,11 +40,11 @@ namespace PetSpa.Modules.Customer.Infrastructure.Persistence.Configurations
             builder.Property(x => x.PasswordHash)
                 .HasColumnName("password_hash")
                 .IsRequired()
-                .HasMaxLength(256);
+                .HasMaxLength(128);
 
             builder.Property(x => x.AvtURL)
                 .HasColumnName("avt_url")
-                .HasMaxLength(256);
+                .HasMaxLength(512);
 
             builder.Property(x => x.Note)
                 .HasColumnName("note");
@@ -57,7 +58,7 @@ namespace PetSpa.Modules.Customer.Infrastructure.Persistence.Configurations
                 .HasColumnName("created_at")
                 .HasColumnType("timestamp with time zone");
 
-            builder.Property(x => x.CreatedAt)
+            builder.Property(x => x.UpdatedAt)
                 .HasColumnName("updated_at")
                 .HasColumnType("timestamp with time zone");
 
